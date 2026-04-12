b49ef18f239a45e8bf3e6d270a90dd9541d650d7
davidedimarco00 <davide.dimarco5@studio.unibo.it>
2026-04-12 13:38:16 +0200

    fix: remove all dependency not used, clean project

--- src/backend/tracking-service/build.gradle.kts
+++ src/backend/tracking-service/build.gradle.kts
@@ -8,13 +8,8 @@ springBoot {
 
 dependencies {
     implementation(libs.spring.boot.starter.web)
-    implementation(libs.spring.boot.starter.webflux)
     implementation(libs.spring.boot.starter.data.mongodb)
 
-    implementation(libs.bundles.vertx.base)
-    implementation(libs.vertx.mongo.client)
-
-    implementation(libs.itext)
 
     implementation(libs.spring.cloud.eureka.client)
 }
