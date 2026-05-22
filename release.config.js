f68dc814cb0134e484aae049f0c785f1ef87fc6d
Danilo Pianini <danilo.pianini@unibo.it>
2026-05-22 16:18:21 +0200

    Add process.exit(1) to release.config.js

--- release.config.js
+++ release.config.js
@@ -1,3 +1,4 @@
+process.exit(1)
 var config = require("semantic-release-preconfigured-conventional-commits");
 config.plugins.push("@semantic-release/github", "@semantic-release/git");
 module.exports = config;
